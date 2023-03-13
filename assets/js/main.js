7e459c5d0b2f1257e0af03884169fd7d6b7b181f
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-13 15:08:09 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// MAIL 
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+
+//GIOCO DEI DADI 
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
